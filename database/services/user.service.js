2cf05f0ffb36706537a8af5dbf4d4f68899b7ad8
Nicole <nschwerin@ltu.edu>
2021-03-08 10:16:01 -0500

    user services

--- database/services/user.service.js
+++ database/services/user.service.js
@@ -1,13 +1,84 @@
 var User = require('../models/user')
 
 
-exports.getUsers = async function (query, page, limit) {
+exports.getUsers = async function (query) {
 
     try {
-        var users = await User.find(query)
-        return users;
+        return await User.find(query).select('-_id -__v');
     } catch (e) {
         // Log Errors
-        throw Error('Error while Paginating Users')
+        throw Error('Error users')
     }
-}
+};
+
+exports.getUsersSSN = async function (query) {
+
+    try {
+        return await User.findOne({ssn: query.params.ssn}).select('-_id -__v');
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
+
+exports.postUsers = async function (query) {
+
+    try {
+        return await User.find(query).save();
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
+
+exports.deleteUsers = async function (query) {
+
+    try {
+        return await User.deleteMany(query);
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
+
+exports.deleteUsersSSN = async function (query) {
+
+    try {
+        return await User.deleteOne({ssn: request.params.ssn});
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
+
+exports.putUsers = async function (query) {
+    const { ssn } = query.params;
+    const user = query.body;
+    user.ssn = ssn;
+
+    try {
+        return await User.findOneAndReplace({ ssn }, user, {
+            upsert: true,
+        });
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
+
+
+exports.patchUsers = async function (query) {
+    const { ssn } = query.params;
+    const user = query.body;
+    user.ssn = ssn;
+
+    try {
+        return await User.findOneAndUpdate({ ssn }, user, {
+            new: true,
+        })
+            .select('-_id -__v');
+    } catch (e) {
+        // Log Errors
+        throw Error('Error users')
+    }
+};
