c3fd8fa087befe7da23bfc9e4c8ba80e8edbcc8d
Nicole <nschwerin@ltu.edu>
2021-03-07 19:06:00 -0500

    correct eslint

--- database/routes/product.routes.js
+++ database/routes/product.routes.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var ProductController = require('../controller/product.controller');
 
-router.get('/', ProductController.getProducts);
-router.get('/:sku', ProductController.getProductSku);
-router.post('/', ProductController.postProducts);
-router.delete('/', ProductController.deleteProducts);
-router.delete('/:sku', ProductController.deleteProductsSku);
-router.put('/:sku', ProductController.putProducts);
-patch.delete('/:sku', ProductController.patchProducts);
+router.get('/product', ProductController.getProducts);
+router.get('/product/:sku', ProductController.getProductSku);
+router.post('/product', ProductController.postProducts);
+router.delete('/product', ProductController.deleteProducts);
+router.delete('/product/:sku', ProductController.deleteProductsSku);
+router.put('/product/:sku', ProductController.putProducts);
+patch.delete('/product/:sku', ProductController.patchProducts);
 
 module.exports = router;
--- database/routes/user.routes.js
+++ database/routes/user.routes.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 var UserController = require('../controller/user.controller');
 
-router.get('/', UserController.getUsers);
-router.get('/:ssn', UserController.getUsersSSN);
-router.post('/', UserController.postUsers);
-router.delete('/', UserController.deleteUsers);
+router.get('/user', UserController.getUsers);
+router.get('/user/:ssn', UserController.getUsersSSN);
+router.post('/user', UserController.postUsers);
+router.delete('/user', UserController.deleteUsers);
 router.delete('/user/:ssn', UserController.deleteUsersSSN);
 router.put('/user/:ssn', UserController.putUsers);
 patch.delete('/user/:ssn', UserController.patchUsers);
