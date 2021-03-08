c4fe3eef4c36253191b38967bc189d3694dd0421
Nicole <nschwerin@ltu.edu>
2021-03-08 10:24:16 -0500

    update product services

--- database/services/product.service.js
+++ database/services/product.service.js
@@ -1,13 +1,84 @@
-var Product = require('../models/product')
+const Product = require('../models/product')
 
 
-exports.getProducts = async function (query, page, limit) {
+exports.getProducts = async function (query) {
 
     try {
-        var products = await Product.find(query)
-        return products;
+        return await Product.find(query).select('-_id -__v');
     } catch (e) {
         // Log Errors
-        throw Error('Error while Paginating Users')
+        throw Error('Error products')
     }
-}
+};
+
+exports.getProductsSku = async function (query) {
+
+    try {
+        return await Product.findOne({sku: query.params.sku}).select('-_id -__v');
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
+
+exports.postProducts = async function (query) {
+
+    try {
+        return await Product.find(query).save();
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
+
+exports.deleteProducts = async function (query) {
+
+    try {
+        return await Product.deleteMany(query);
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
+
+exports.deleteProductSku = async function (query) {
+
+    try {
+        return await User.deleteOne({sku: query.params.sku});
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
+
+exports.putProducts = async function (query) {
+    const { sku } = query.params;
+    const product = query.body;
+    product.sku = sku;
+
+    try {
+        return await Product.findOneAndReplace({ sku }, product, {
+            upsert: true,
+        });
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
+
+
+exports.patchProducts = async function (query) {
+    const { sku } = query.params;
+    const product = query.body;
+    product.sku = sku;
+
+    try {
+        return await User.findOneAndUpdate({ sku }, product, {
+            new: true,
+        })
+            .select('-_id -__v');
+    } catch (e) {
+        // Log Errors
+        throw Error('Error products')
+    }
+};
--- database/services/user.service.js
+++ database/services/user.service.js
@@ -1,4 +1,4 @@
-var User = require('../models/user')
+const User = require('../models/user')
 
 
 exports.getUsers = async function (query) {
